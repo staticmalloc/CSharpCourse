d542c83ef7c7863e9b5e992680487ecd3bbb9bf4
staticmalloc <s.afanasev@g.nsu.ru>
2019-05-20 15:38:02 +0700

    Branch ficha added

---
+++ AppFour/Migrations/20190520083127_SecondBranch.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppFour.Migrations
+{
+    public partial class SecondBranch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Speciality",
+                table: "Workers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Speciality",
+                table: "Workers");
+        }
+    }
+}
